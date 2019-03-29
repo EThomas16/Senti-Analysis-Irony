c55b349cb91b42a40e41aa66d1b10557aad206e3
Fyrngarm <erikthomas146@gmail.com>
2019-03-29 18:15:31 +0000

    Fixed issue with minmaxscaler not working

--- vectorisers.py
+++ vectorisers.py
@@ -1,16 +1,18 @@
 import nlp_utils
 import numpy as np
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+from gensim.models.ldamodel import LdaModel
 
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.model_selection import train_test_split, StratifiedShuffleSplit, StratifiedKFold
-from sklearn.preprocessing import normalize, Normalizer, MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler
 from sklearn import naive_bayes, svm, ensemble
 
 from decorators import reset_file
 
 #np.set_printoptions(threshold=np.nan)
 
-def generate_doc2vec_model(features: list, vec_size: int = 5) -> object:
+def generate_doc2vec_model(features: list, vec_size: int = 200) -> object:
     # documents = [TaggedDocument(doc, [idx]) for idx, doc in enumerate(features)]
     documents = []
     for idx, doc in enumerate(features):
@@ -35,7 +37,7 @@ if __name__ == "__main__":
         "Data/Sarcasm_Headlines_Dataset.json", "headline", "is_sarcastic")
     
     features = nlp_utils.stem_words(features)
-    model = generate_doc2vec_model(features)
+    model = generate_doc2vec_model(features, vec_size=300)
     # model.save("Data/models/headlines_test_model.d2v")
     # model = Doc2Vec.load("Data/models/headlines_test_model.d2v")    
     doc_vectors = generate_doc2vec_vectors(model)
@@ -44,24 +46,27 @@ if __name__ == "__main__":
 
     doc_vectors = np.array(doc_vectors)
     scaler = MinMaxScaler(feature_range=(0, 1))
-    scaler.fit_transform(doc_vectors)
-    # doc_vectors = normalize(doc_vectors, axis=1)
-    # doc_vectors = Normalizer(copy=False).fit_transform(doc_vectors)
+    doc_vectors = scaler.fit_transform(doc_vectors)
     # bow_features = np.array(bow_features.toarray())
     tfidf_features = np.array(tfidf_features.toarray())
     concatenated_features = nlp_utils.concatenate_features((doc_vectors, tfidf_features))
-
-    clf = naive_bayes.BernoulliNB()
+    clf = naive_bayes.MultinomialNB()
     # clf = svm.LinearSVC()
+    # clf = LinearDiscriminantAnalysis()
     data = nlp_utils.TextData()
     data.X = concatenated_features
     data.y = np.array(labels)
     
-    shuffle_split = StratifiedShuffleSplit(n_splits=1)
-    skf = StratifiedKFold(n_splits=10)
-    scores, times = data.stratify(skf, clf, eval_metric="f-score")
-    
-    results_file_path = "Results/concatenated_results_tfidf_d2v_skf_stem.csv"
+    # shuffle_split = StratifiedShuffleSplit(n_splits=1)
+    seeds = [6, 40, 32, 17, 19]
+    all_scores = []; all_times = []
+    for seed in seeds:
+        skf = StratifiedKFold(n_splits=2, random_state=seed)
+        scores, times = data.stratify(skf, clf, eval_metric="f-score")
+        all_scores.extend([scores[0], scores[1]])
+        all_times.extend([times[0], times[1]])
+
+    results_file_path = "Results/d2v_skf_stem.csv"
     # reset_file(results_file_path, "Classifier,Score,Fold,Execution Time")
-    for idx, (score, time) in enumerate(zip(scores, times)):
-        nlp_utils.write_results_stratification(results_file_path, "Bernoulli NB", score, idx+1, time)
+    for idx, (score, time) in enumerate(zip(all_scores, all_times)):
+        nlp_utils.write_results_stratification(results_file_path, "Multinomial NB", score, idx+1, time)
